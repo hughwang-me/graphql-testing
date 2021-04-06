709676f31eb14b46d02278591e237bb8e5e38b71
wanghuan <18501667737@163.com>
2021-04-06 21:58:46 +0800

    init

---
+++ src/main/java/com/uwjx/graphql/GraphqlTestingApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/graphql/GraphqlTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
